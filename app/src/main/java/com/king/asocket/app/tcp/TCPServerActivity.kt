0efc2e628ab676ef0c5fc86c73c9994b7876ed98
Youngfellows <Youngfellows@163.com>
2022-06-30 10:18:16 +0800

    xxxx

--- app/src/main/java/com/king/asocket/app/tcp/TCPServerActivity.kt
+++ app/src/main/java/com/king/asocket/app/tcp/TCPServerActivity.kt
@@ -44,12 +44,12 @@ class TCPServerActivity : AppCompatActivity() {
 
     private fun getContext() = this
 
-    private fun clickStart(){
+    private fun clickStart() {
         mPort = binding.etPort.text.toString().toInt()
         val client = TCPServer(mPort)
         aSocket = ASocket(client)
         aSocket?.let {
-            it.setOnSocketStateListener(object : ISocket.OnSocketStateListener{
+            it.setOnSocketStateListener(object : ISocket.OnSocketStateListener {
                 override fun onStarted() {
                     binding.btnStart.isEnabled = false
                     binding.etPort.isEnabled = false
@@ -63,9 +63,9 @@ class TCPServerActivity : AppCompatActivity() {
 
                 override fun onException(e: Exception) {
 
-                    if(!it.isConnected){//连接失败
+                    if (!it.isConnected) {//连接失败
                         LogUtils.d("连接失败")
-                        Toast.makeText(getContext(),"连接失败", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(getContext(), "连接失败", Toast.LENGTH_SHORT).show()
                         binding.btnStart.isEnabled = true
                         binding.etPort.isEnabled = true
                         binding.progressBar.isVisible = false
@@ -76,6 +76,9 @@ class TCPServerActivity : AppCompatActivity() {
             })
             it.setOnMessageReceivedListener { data ->
                 binding.tvContent.append("接收：${String(data)}\n")
+//                var text: String = binding.tvContent.text.toString()
+//                text += "接收：${String(data)}\n"
+//                binding.tvContent.text = text
             }
             binding.progressBar.isVisible = true
             it.start()
@@ -83,12 +86,12 @@ class TCPServerActivity : AppCompatActivity() {
         }
     }
 
-    private fun clickSend(){
-        if(!TextUtils.isEmpty(binding.etContent.text)){
+    private fun clickSend() {
+        if (!TextUtils.isEmpty(binding.etContent.text)) {
             aSocket?.let {
                 val data = binding.etContent.text.toString()
                 it.write(data.toByteArray())
-                if(it.isStart){
+                if (it.isStart) {
                     binding.tvContent.append("发送：${data}\n")
                 }
                 binding.etContent.setText("")
@@ -96,12 +99,12 @@ class TCPServerActivity : AppCompatActivity() {
         }
     }
 
-    private fun clickClear(){
+    private fun clickClear() {
         binding.tvContent.text = ""
     }
 
-    fun onClick(v: View){
-        when(v.id){
+    fun onClick(v: View) {
+        when (v.id) {
             R.id.btnStart -> clickStart()
             R.id.btnSend -> clickSend()
             R.id.btnClear -> clickClear()
